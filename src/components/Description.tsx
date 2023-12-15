179a0f7d22fc7e5e3c15cf8df80cad0ef288abbb
Erudite885 <erudite885@gmail.com>
2023-12-15 20:25:31 +1300

    description component

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Description: React.FC = () => {
   return (
-    <div>Description</div>
+    <div className='text-white md:w-[50%] flex flex-col pt-20 md:pt-0'>
+      <h2 className='text-5xl font-medium font-sans text-center md:text-start'>Learn to code by watching others</h2>
+      <p className=' py-5 text-lg text-slate-100 max-w-sm'>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+    </div>
   )
 }
 
