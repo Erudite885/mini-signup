6cc5c9e5843ebbb6f938842b9933de6b90d8d071
Erudite885 <erudite885@gmail.com>
2023-12-18 19:50:06 +1300

    app styling updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SignUpComponent from './components/SignUpComponent'
 
 const App:  React.FC = () => {
   return (
-    <div className='flex items-center justify-center w-[100vw] h-[100vh] bg-rose-500'>
+    <div className='flex items-center justify-center h-[100vh] bg-rose-500'>
       <SignUpComponent />
     </div>
   )
